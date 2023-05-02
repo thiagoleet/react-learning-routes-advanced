b94ef77b4dbf2fa5c2f95edfe9e6d538983ef251
Thiago Leite <thiago.leite@agileengine.com>
2023-05-02 12:49:39 -0300

    - Added Root page - Added child routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,14 +16,21 @@ import {
   EventsPage,
   HomePage,
   NewEventPage,
+  Root,
 } from "./pages";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/events", element: <EventsPage /> },
-  { path: "/events/:eventId", element: <EventDetailPage /> },
-  { path: "/events/new", element: <NewEventPage /> },
-  { path: "/events/:eventId/edit", element: <EditEventPage /> },
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 const App = () => {
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,3 +3,4 @@ export * from "./EventsPage";
 export * from "./EventDetailPage";
 export * from "./HomePage";
 export * from "./NewEventPage";
+export * from "./Root";
