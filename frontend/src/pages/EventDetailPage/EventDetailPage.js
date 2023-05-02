2563571a048372fb3299c02d973e8741e7e11520
Thiago Leite <thiago.leite@agileengine.com>
2023-05-02 13:23:00 -0300

    Output the ID of the selected event on the EventDetailPage

--- frontend/src/pages/EventDetailPage/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage/EventDetailPage.js
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const EventDetailPage = () => {
-  return <h1>EventDetailPage</h1>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>EventDetailPage</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
 };
 
 export default EventDetailPage;
