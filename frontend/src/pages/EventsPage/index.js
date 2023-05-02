0f298ac1e4c7509c73144c8a698adfedd7fd78c6
Thiago Leite <thiago.leite@agileengine.com>
2023-05-02 15:46:33 -0300

    Fix events route

--- frontend/src/pages/EventsPage/index.js
+++ frontend/src/pages/EventsPage/index.js
@@ -1,7 +1,7 @@
 import EventsPage from "./EventsPage";
 
 const eventsLoader = async () => {
-  const response = await fetch("http://localhost:8080/errr");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
