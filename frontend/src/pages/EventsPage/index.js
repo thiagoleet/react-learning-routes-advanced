c1359fd641ac45f2b79cff21c06b3064c34c10e2
Thiago Leite <thiago.leite@agileengine.com>
2023-05-02 15:50:32 -0300

    Improve the error returning

--- frontend/src/pages/EventsPage/index.js
+++ frontend/src/pages/EventsPage/index.js
@@ -1,12 +1,11 @@
+import { json } from "react-router-dom";
 import EventsPage from "./EventsPage";
 
 const eventsLoader = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    });
+    return json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     return response;
   }
