98886769ad779663ef1f7d1746a555e1a9b59a30
Thiago Leite <thiago.leite@agileengine.com>
2023-05-02 15:27:23 -0300

    Using useNavigation hook

--- frontend/src/pages/Root/Root.js
+++ frontend/src/pages/Root/Root.js
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import { MainNavigation } from "../../components";
 
 const RootLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {navigation.state === "loading" && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
